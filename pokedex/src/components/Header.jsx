5c4e35f889227425ed30c7c05192cd0fb29b386b
Alex24nest <anesterov2404@gmail.com>
2025-02-23 16:09:35 -0500

    Update Header.jsx
    add something

--- pokedex/src/components/Header.jsx
+++ pokedex/src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header(props) {
    
   return (
     <header>
-      <button onClick={handleToggleMenu} className="open-nav-button">
+      <button onClick={handleToggleMenu} className="open-nav-button type-container">
         <i className="fa-solid fa-bars"></i>
         <h1 className="text-gradient">Pokedex</h1>
       </button>
