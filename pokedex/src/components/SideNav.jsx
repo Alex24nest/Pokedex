ed8adc43ac6f5fba86bd4b0497a1c99d75aa786c
Alex Nesterov <anesterov2404@gmail.com>
2025-02-23 15:47:04 -0500

    1

--- pokedex/src/components/SideNav.jsx
+++ pokedex/src/components/SideNav.jsx
@@ -4,9 +4,9 @@ import { first151Pokemon, getFullPokedexNumber } from '../utils'
  export default function SideNav(props) {
   const { selectedPokemon, setSelectedPokemon, handleCloseMenu, showSideMenu } = props 
 
-  const [ searchValue, setSearchValye ] = useState('')
+  const [ searchValue, setSearchValue ] = useState('')
 
-  const filteredPakemon = first151Pokemon.filter((ele, eleIndex) => {
+  const filteredPokemon = first151Pokemon.filter((ele, eleIndex) => {
     if (getFullPokedexNumber(eleIndex).includes(searchValue)) { return true }
 
     if (ele.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())) { return true }
@@ -23,16 +23,15 @@ import { first151Pokemon, getFullPokedexNumber } from '../utils'
         <h1 className='text-gradient'>Pokedex</h1>
         </div>
         <input placeholder='E.g. 001 or Bulda...' value={searchValue} onChange={(e) => {
-          setSearchValye(e.target.value)
+          setSearchValue(e.target.value)
         }} />
-      {filteredPakemon .map((pokemon, pokemonIndex) => {
+      {filteredPokemon.map((pokemon, pokemonIndex) => {
         const truePokedexNumber = first151Pokemon.indexOf(pokemon)
         return (
         <button onClick={() => {
           setSelectedPokemon(truePokedexNumber)
           handleCloseMenu()
-        }} key={pokemonIndex} className={'nav-card ' 
-          + (pokemonIndex === selectedPokemon ? ' nav-card-selected' : ' ')}>
+        }} key={pokemonIndex} className={'nav-card ' + (pokemonIndex === selectedPokemon ? ' nav-card-selected' : ' ')}>
           <p>{getFullPokedexNumber(truePokedexNumber)}</p>
           <p>{pokemon}</p>
         </button>
