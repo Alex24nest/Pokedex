5470c6197430e727edb06813734c9f95389c1553
Alex Nesterov <anesterov2404@gmail.com>
2025-02-23 15:51:33 -0500

    full

--- pokedex/src/components/TypeCard.jsx
+++ pokedex/src/components/TypeCard.jsx
@@ -1,12 +1,12 @@
-import { getFullPokedexNumber, pokemonTypeColors } from "../utils"
+import { pokemonTypeColors } from "../utils"
 
 
 export default function TypeCard(props) {
   const { type } = props
 
   return (
-    <div className="type-tile"style={{ color: pokemonTypeColors?.[type]?.color, background: pokemonTypeColors?.[type]?.background }} >
-      <p>{type}</p>
+    <div className="type-tile" style={{ color: pokemonTypeColors?.[type]?.color, background: pokemonTypeColors?.[type]?.background }} >
+      <p>{type} </p>
     </div>
   ) 
 }
