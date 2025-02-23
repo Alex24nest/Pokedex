e38e9b1863ebc3622632aff98eb161a1f085283c
Alex Nesterov <anesterov2404@gmail.com>
2025-02-23 15:50:38 -0500

    1

--- pokedex/src/components/SideNav.jsx
+++ pokedex/src/components/SideNav.jsx
@@ -7,22 +7,22 @@ import { first151Pokemon, getFullPokedexNumber } from '../utils'
   const [ searchValue, setSearchValue ] = useState('')
 
   const filteredPokemon = first151Pokemon.filter((ele, eleIndex) => {
-    if (getFullPokedexNumber(eleIndex).includes(searchValue)) { return true }
+    if ((getFullPokedexNumber(eleIndex)).includes(searchValue)) { return true }
 
-    if (ele.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())) { return true }
+    if (ele.toLowerCase().includes(searchValue.toLowerCase())) { return true }
 
     return false
   })
 
-  return(
+  return (
     <nav className={' ' + (!showSideMenu ? ' open' : '')}> 
-      <div className={'header  ' + (!showSideMenu ? ' open' : '')}>
+      <div className={'header ' + (!showSideMenu ? ' open' : '')}>
         <button onClick={handleCloseMenu} className='open-nav-button'>
            <i className ="fa-solid fa-arrow-left-long"></i>
         </button>
         <h1 className='text-gradient'>Pokedex</h1>
         </div>
-        <input placeholder='E.g. 001 or Bulda...' value={searchValue} onChange={(e) => {
+        <input placeholder='E.g. 001 or Bulba...' value={searchValue} onChange={(e) => {
           setSearchValue(e.target.value)
         }} />
       {filteredPokemon.map((pokemon, pokemonIndex) => {
@@ -31,7 +31,8 @@ import { first151Pokemon, getFullPokedexNumber } from '../utils'
         <button onClick={() => {
           setSelectedPokemon(truePokedexNumber)
           handleCloseMenu()
-        }} key={pokemonIndex} className={'nav-card ' + (pokemonIndex === selectedPokemon ? ' nav-card-selected' : ' ')}>
+        }} key={pokemonIndex} className={'nav-card ' 
+          + (pokemonIndex === selectedPokemon ? ' nav-card-selected' : ' ')}>
           <p>{getFullPokedexNumber(truePokedexNumber)}</p>
           <p>{pokemon}</p>
         </button>
